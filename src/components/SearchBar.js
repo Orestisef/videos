b5ff214382e9d1e99ab8c5a75caa7f2cd13a13fb
norest <orestisef@gmail.com>
2019-09-14 15:30:56 +0300

    Fetched data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,8 +12,8 @@ class SearchBar extends React.Component {
   onFormSubmit = event => {
     //the default action of the event will not be triggered
     event.preventDefault();
-    //TODO: Make sure we call
-    // callback from parent component
+
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
